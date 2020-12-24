36dd3211cf67743c675b38f25847e66585cd4b6f
Iain Stewart <iain.stewart08@gmail.com>
2020-12-24 01:39:03 +0000

    Add DbContext with character DbSet

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using CoreRPG.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreRPG.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
