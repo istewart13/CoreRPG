17314818d6a11b50a45ef21d0051cc6fb6f3d590
Iain Stewart <iain.stewart.08@gmail.com>
2021-01-07 21:16:47 +0000

    Implement register method

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -19,12 +19,27 @@ namespace CoreRPG.Data
 
         public async Task<int> Register(User user, string password)
         {
-            throw new System.NotImplementedException();
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user.Id;
         }
 
         public Task<bool> UserExists(string username)
         {
             throw new System.NotImplementedException();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
